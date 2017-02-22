07fa1acc851324ff52459dfda2cb5d028c476f15
Daniel Hutchinson <danielhutchinson5@gmail.com>
2017-02-22 14:27:16 +0000

    add a couple of base tests to make sure all of the testing setup is now working

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,18 +1,19 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
 
-describe('App', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({ declarations: [AppComponent]});
-  });
-  it ('should work', () => {
-    let fixture = TestBed.createComponent(AppComponent);
-    expect(fixture.componentInstance instanceof AppComponent).toBe(true, 'should create AppComponent');
-  });
+describe('When opening the application', () => {
+    beforeEach(() => {
+        TestBed.configureTestingModule({ declarations: [AppComponent] });
+    });
+
+    it('It bootstraps the AppComponent', () => {
+        let fixture = TestBed.createComponent(AppComponent);
+        expect(fixture.componentInstance instanceof AppComponent).toBe(true);
+    });
+
+    it('It displays a title', () => {
+        let fixture = TestBed.createComponent(AppComponent);
+        let element = fixture.nativeElement;
+        expect(element.querySelector('h1').innerHTML).toBe('Hello from Angular App with Webpack');
+    });
 });
-
-// describe('A test', () => {
-//     it('passes', () => {
-//         expect(true).toBe(false);
-//     });
-// });
