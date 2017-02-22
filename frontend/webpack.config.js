1381d686dcb7f8b671902549f83e980092e4572f
Elliot Evans <elliot@Elliots-MacBook-Pro.local>
2017-02-22 08:57:06 +0000

    added a test webpack file, updated package json and updated yarn lock

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,6 +2,9 @@ switch (process.env.NODE_ENV) {
   case 'prod':
     module.exports = require('./webpack.prod');
     break;
+  case 'test':
+    module.exports = require('./webpack.test');
+    break;
   default:
     module.exports = require('./webpack.dev');
 }
---
+++ frontend/webpack.test.js
@@ -0,0 +1,80 @@
+'use strict';
+
+// Plugin / Base Require
+const webpack                   = require('webpack');
+const path                      = require('path');
+const helpers                   = require('./config/helpers');
+
+// Webpack Variables
+const srcPath                   = path.join(__dirname, './src');
+const srcApp                    = path.join(__dirname, './src/main.ts');
+const srcPollyfill              = path.join(__dirname, './src/polyfills.ts');
+const srcVendor                 = path.join(__dirname, './src/vendor.ts');
+const distPath                  = path.join(__dirname, './dist');
+
+module.exports = {
+    context: srcPath,
+
+    // SOURCEMAPS
+    devtool: 'source-map',
+
+    // START POINT OF BUNDLER
+    entry: {
+    polyfills: srcPollyfill,
+    app: srcApp,
+    vendor: srcVendor
+    },
+
+    // OUTPUT OF WEBPACK
+    output: {
+    path: distPath,
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[name].map',
+    chunkFilename: '[id].chunk.js',
+    publicPath: '/'
+    },
+
+    // MODULES
+    module: {
+        rules: [
+            {
+                // TS
+                test: /\.(ts)$/,
+                exclude: /node_modules/,
+                loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+            }
+        ]
+    },
+    
+    // RESOLVE
+    resolve: {
+        extensions: ['.js', '.ts'],
+        modules: [
+            path.resolve(__dirname, 'node_modules'),
+            srcPath
+        ]
+    },
+
+    // performance
+    performance: {
+        hints: "warning"
+    },
+
+    // the environment in which the bundle should run
+    target: "node",
+
+    // PLUGINS
+    plugins: [
+        // Workaround for angular/angular#11580
+        // New solution is to target is non existing folder
+        new webpack.ContextReplacementPlugin(
+            /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+            path.resolve(__dirname, 'doesnotexist/')
+        ),
+
+        // Names the output files
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor', 'polyfills']
+        }),
+    ],
+};
